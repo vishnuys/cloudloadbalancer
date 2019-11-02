d903b807d33456cdff4b0c08899f9bf95b1c234e
Vishnu Y S <ysv8152@gmail.com>
2019-11-02 10:54:52 -0700

    Modularize and reuse functions, update requirements

--- loader/helper.py
+++ loader/helper.py
@@ -1,20 +1,65 @@
 import os
 import requests
+from uhashring import HashRing
 from traceback import format_exc
-from clouder.settings import NODE_LIST, NODE_ADDRESS
+from mmh3 import hash as mmh
+from clouder.settings import NODE_LIST, NODE_ADDRESS, REPLICATION_FACTOR
+
 
 def status_check():
-	result = {}
-	for i in NODE_LIST:
-		try:
-			addr = os.path.join(NODE_ADDRESS[i], 'status/')
-			r = requests.get(addr)
-			if r.ok:
-				result[i] = 'Live'
-			else:
-				result[i] = 'Down'
+    result = {}
+    for i in NODE_LIST:
+        try:
+            addr = os.path.join(NODE_ADDRESS[i], 'status/')
+            r = requests.get(addr)
+            if r.ok:
+                result[i] = 'Live'
+            else:
+                result[i] = 'Down'
 
-		except Exception:
-			print(format_exc())
-			result[i] = 'Down'
-	return result
+        except requests.exceptions.RequestException:
+            print(i, 'is down')
+        except Exception:
+            print(format_exc())
+            result[i] = 'Down'
+    return result
+
+
+def node_to_contact(name):
+    node_status = status_check()
+    alive_nodes = [x for x in NODE_LIST if node_status[x] == 'Live']
+    hr = HashRing(nodes=alive_nodes, hash_fn=mmh)
+    node = hr.get_node(name)
+    print(node)
+    return node
+
+
+def handle_result(req, node):
+    print("HTTP STATUS CODE = %d" % req.status_code)
+    response = ''
+    if req.ok:
+        res = req.json()
+        replication_count = res['count']
+        node_result = res['result']
+        print('result=%s count=%d' % (node_result, replication_count))
+        if replication_count >= REPLICATION_FACTOR:
+            result = {
+                'status': 'success',
+                'node': node,
+            }
+            response = "SUCCESS"
+            return result, response
+        elif replication_count == 0:
+            response = "BAD_REQUEST"
+            return node_result, response
+        elif replication_count < REPLICATION_FACTOR:
+            result = {
+                'status': 'Failed to write in majority nodes',
+                'node': node,
+            }
+            response = "SUCCESS"
+            return result, response
+    else:
+        result = "Something went wrong"
+        response = "SERVER_ERROR"
+        return result, response
--- loader/views.py
+++ loader/views.py
@@ -1,251 +1,145 @@
 import os
 import requests
-from uhashring import HashRing
-from traceback import format_exc
-from .helper import status_check
+from .helper import status_check, node_to_contact, handle_result
 from django.shortcuts import render
 from IPython import embed
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.datastructures import MultiValueDictKeyError
-from clouder.settings import NODE_LIST, NODE_ADDRESS, REPLICATION_FACTOR
-from django.http import HttpResponse, JsonResponse, HttpResponseServerError, HttpResponseBadRequest
+from clouder.settings import NODE_ADDRESS
+from django.http import JsonResponse, HttpResponseServerError, HttpResponseBadRequest
 
 
 # Create your views here.
 class DefaultView(TemplateView):
 
-	def get(self, request):
-		return render(request, "index.html")
+    def get(self, request):
+        return render(request, "index.html")
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateBucket(TemplateView):
 
-	def post(self, request):
-		name = request.POST.get('name')
-		node_status = status_check()
-		alive_nodes = [x for x in NODE_LIST if node_status[x]=='Live']
-		hr = HashRing(nodes=alive_nodes)
-		print(hr, type(hr))
-		node = hr.get_node(name)
-		addr = os.path.join(NODE_ADDRESS[node], 'createbucket/')
-		print(node)
-		data = {'name': name}
-		r = requests.post(addr, data=data)
-		print("HTTP STATUS CODE = %d" % r.status_code)
-		if r.ok:
-			res = r.json()
-			replication_count = res['count']
-			node_result = res['result']
-			print('result=%s count=%d' % (node_result, replication_count))
-			if replication_count >= REPLICATION_FACTOR:
-				result = {
-					'status': 'success',
-					'node': node,
-				}
-				return JsonResponse(result)
-			elif replication_count == 0:
-				return HttpResponseBadRequest(node_result)
-			elif replication_count < REPLICATION_FACTOR:
-				result = {
-					'status': 'failure to write in majority nodes',
-					'node': node,
-				}
-				return JsonResponse(result)
-		else:
-			return HttpResponseServerError("Something went wrong")
+    def post(self, request):
+        name = request.POST.get('name')
+        if name == '':
+            return HttpResponseBadRequest('Please enter valid bucket name')
+        node = node_to_contact(name)
+        addr = os.path.join(NODE_ADDRESS[node], 'createbucket/')
+        data = {'name': name}
+        r = requests.post(addr, data=data)
+        result, response = handle_result(r, node)
+        if response == 'SUCCESS':
+            return JsonResponse(result)
+        elif response == 'BAD_REQUEST':
+            return HttpResponseBadRequest(result)
+        elif response == 'SERVER_ERROR':
+            return HttpResponseServerError(result)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class DeleteBucket(TemplateView):
 
-	def post(self, request):
-		name = request.POST.get('name')
-		node_status = status_check()
-		alive_nodes = [x for x in NODE_LIST if node_status[x]=='Live']
-		hr = HashRing(nodes=alive_nodes)
-		print(hr, type(hr))
-		node = hr.get_node(name)
-		addr = os.path.join(NODE_ADDRESS[node], 'deletebucket/')
-		print(node)
-		data = {'name': name}
-		r = requests.post(addr, data=data)
-		print("HTTP STATUS CODE = %d" % r.status_code)
-		if r.ok:
-			res = r.json()
-			replication_count = res['count']
-			node_result = res['result']
-			print('result=%s count=%d' % (node_result, replication_count))
-			if replication_count >= REPLICATION_FACTOR:
-				result = {
-					'status': 'success',
-					'node': node,
-				}
-				return JsonResponse(result)
-			elif replication_count == 0:
-				return HttpResponseBadRequest(node_result)
-			elif replication_count < REPLICATION_FACTOR:
-				result = {
-					'status': 'failure to write in majority nodes',
-					'node': node,
-				}
-				return JsonResponse(result)
-		else:
-			return HttpResponseServerError("Something went wrong")
+    def post(self, request):
+        name = request.POST.get('name')
+        if name == '':
+            return HttpResponseBadRequest('Please enter valid bucket name')
+        node = node_to_contact(name)
+        data = {'name': name}
+        addr = os.path.join(NODE_ADDRESS[node], 'deletebucket/')
+        r = requests.post(addr, data=data)
+        result, response = handle_result(r, node)
+        if response == 'SUCCESS':
+            return JsonResponse(result)
+        elif response == 'BAD_REQUEST':
+            return HttpResponseBadRequest(result)
+        elif response == 'SERVER_ERROR':
+            return HttpResponseServerError(result)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class StatusChecker(TemplateView):
 
-	def post(self, request):
-		result = status_check()
-		return JsonResponse(result)
+    def post(self, request):
+        result = status_check()
+        return JsonResponse(result)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateFile(TemplateView):
 
-	def post(self, request):
-		try:
-			file = request.FILES['file']
-			name = request.POST['name']
-			bucket = request.POST['bucket']
-		except MultiValueDictKeyError:
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
-		if name == '' or bucket == '':
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+    def post(self, request):
+        try:
+            file = request.FILES['file']
+            name = request.POST['name'].strip()
+            bucket = request.POST['bucket'].strip()
+        except MultiValueDictKeyError:
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+        if name == '' or bucket == '':
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
 
-		node_status = status_check()
-		alive_nodes = [x for x in NODE_LIST if node_status[x]=='Live']
-		hr = HashRing(nodes=alive_nodes)
-		print(hr, type(hr))
-		node = hr.get_node(name)
-		addr = os.path.join(NODE_ADDRESS[node], 'createfile/')
-		print(node)
-		data = {'name': name, 'bucket': bucket}
-		filedata = {'file': file}
-		r = requests.post(addr, data=data, files=filedata)
-		print("HTTP STATUS CODE = %d" % r.status_code)
-		if r.ok:
-			res = r.json()
-			replication_count = res['count']
-			node_result = res['result']
-			vector_clocks = res['vector_clocks']
-			print('result=%s count=%d' % (node_result, replication_count))
-			if replication_count >= REPLICATION_FACTOR:
-				result = {
-					'status': 'success',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-			elif replication_count == 0:
-				return HttpResponseBadRequest(node_result)
-			elif replication_count < REPLICATION_FACTOR:
-				result = {
-					'status': 'failure to write in majority nodes',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-		else:
-			return HttpResponseServerError("Something went wrong")
+        node = node_to_contact(name)
+        data = {'name': name, 'bucket': bucket}
+        addr = os.path.join(NODE_ADDRESS[node], 'createfile/')
+        filedata = {'file': file}
+        r = requests.post(addr, data=data, files=filedata)
+        result, response = handle_result(r, node)
+        if response == 'SUCCESS':
+            return JsonResponse(result)
+        elif response == 'BAD_REQUEST':
+            return HttpResponseBadRequest(result)
+        elif response == 'SERVER_ERROR':
+            return HttpResponseServerError(result)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class DeleteFile(TemplateView):
 
-	def post(self, request):
-		try:
-			name = request.POST['name']
-			bucket = request.POST['bucket']
-			file = request.FILES['file']
-		except MultiValueDictKeyError:
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
-		if name == '' or bucket == '':
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+    def post(self, request):
+        try:
+            name = request.POST['name'].strip()
+            bucket = request.POST['bucket'].strip()
+        except MultiValueDictKeyError:
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+        if name == '' or bucket == '':
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
 
-		node_status = status_check()
-		alive_nodes = [x for x in NODE_LIST if node_status[x]=='Live']
-		hr = HashRing(nodes=alive_nodes)
-		print(hr, type(hr))
-		node = hr.get_node(name)
-		addr = os.path.join(NODE_ADDRESS[node], 'updatefile/')
-		print(node)
-		data = {'name': name, 'bucket': bucket}
-		r = requests.post(addr, data=data)
-		print("HTTP STATUS CODE = %d" % r.status_code)
-		if r.ok:
-			res = r.json()
-			replication_count = res['count']
-			node_result = res['result']
-			vector_clocks = res['vector_clocks']
-			print('result=%s count=%d' % (node_result, replication_count))
-			if replication_count >= REPLICATION_FACTOR:
-				result = {
-					'status': 'success',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-			elif replication_count == 0:
-				return HttpResponseBadRequest(node_result)
-			elif replication_count < REPLICATION_FACTOR:
-				result = {
-					'status': 'failure to write in majority nodes',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-		else:
-			return HttpResponseServerError("Something went wrong")
+        node = node_to_contact(name)
+        data = {'name': name, 'bucket': bucket}
+        addr = os.path.join(NODE_ADDRESS[node], 'deletefile/')
+        r = requests.post(addr, data=data)
+        result, response = handle_result(r, node)
+        if response == 'SUCCESS':
+            return JsonResponse(result)
+        elif response == 'BAD_REQUEST':
+            return HttpResponseBadRequest(result)
+        elif response == 'SERVER_ERROR':
+            return HttpResponseServerError(result)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UpdateFile(TemplateView):
 
-	def post(self, request):
-		try:
-			name = request.POST['name']
-			bucket = request.POST['bucket']
-		except MultiValueDictKeyError:
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
-		if name == '' or bucket == '':
-			return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+    def post(self, request):
+        try:
+            name = request.POST['name'].strip()
+            bucket = request.POST['bucket'].strip()
+            file = request.FILES['file']
+        except MultiValueDictKeyError:
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
+        if name == '' or bucket == '':
+            return HttpResponseBadRequest('Please enter valid name, bucket and select a valid file to upload')
 
-		node_status = status_check()
-		alive_nodes = [x for x in NODE_LIST if node_status[x]=='Live']
-		hr = HashRing(nodes=alive_nodes)
-		print(hr, type(hr))
-		node = hr.get_node(name)
-		addr = os.path.join(NODE_ADDRESS[node], 'deletefile/')
-		print(node)
-		data = {'name': name, 'bucket': bucket}
-		r = requests.post(addr, data=data)
-		print("HTTP STATUS CODE = %d" % r.status_code)
-		if r.ok:
-			res = r.json()
-			replication_count = res['count']
-			node_result = res['result']
-			vector_clocks = res['vector_clocks']
-			print('result=%s count=%d' % (node_result, replication_count))
-			if replication_count >= REPLICATION_FACTOR:
-				result = {
-					'status': 'success',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-			elif replication_count == 0:
-				return HttpResponseBadRequest(node_result)
-			elif replication_count < REPLICATION_FACTOR:
-				result = {
-					'status': 'failure to write in majority nodes',
-					'node': node,
-					'vector_clocks': vector_clocks
-				}
-				return JsonResponse(result)
-		else:
-			return HttpResponseServerError("Something went wrong")
+        node = node_to_contact(name)
+        data = {'name': name, 'bucket': bucket}
+        filedata = {'file': file}
+        addr = os.path.join(NODE_ADDRESS[node], 'updatefile/')
+        r = requests.post(addr, data=data, files=filedata)
+        result, response = handle_result(r)
+        if response == 'SUCCESS':
+            return JsonResponse(result)
+        elif response == 'BAD_REQUEST':
+            return HttpResponseBadRequest(result)
+        elif response == 'SERVER_ERROR':
+            return HttpResponseServerError(result)
